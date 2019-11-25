62acf2c20c4979e9310848df2a6bb95c1a84df14
crodriguez1a <crodriguez1a@gmail.com>
2019-11-24 21:10:53 -0500

    Additional cleanup

--- models/a.py
+++ models/a.py
@@ -3,10 +3,9 @@ import os
 from dataclasses import dataclass, field
 
 import numpy as np
-import keras
+import keras # TODO: update to tf.keras when kapre goes to tf2.0
 
 import librosa
-import soundfile as sf
 from kapre.time_frequency import Spectrogram
 
 """
@@ -49,7 +48,8 @@ def summarize_compile(model: keras.Model):
                   metrics=[ # @paper: 1) Mean Percentile Rank?
                             keras.metrics.MeanAbsolutePercentageError(),
                             # @paper: 2) Top-k mean accuracy based evaluation
-                            # TODO:  metrics.TopKCategoricalAccuracy(),
+                            # TODO: keras.metrics.TopKCategoricalAccuracy(),
+                            # https://github.com/tensorflow/tensorflow/issues/9243
                             # @paper: 3) Mean Absolute Error based evaluation
                             keras.metrics.MeanAbsoluteError(),])
 
@@ -86,7 +86,7 @@ def predict(model: keras.Model,
     # predict
     result: np.ndarray = model.predict(x=x)
 
-    # shuffle for `channels first`
+    # rearrange for `channels first`
     is_channels_first: bool = keras.backend.image_data_format == 'channels_first'
     result = result[0, 0] if is_channels_first else result[0, :, :, 0]
 
@@ -106,12 +106,12 @@ class ArchLayer:
     strides: tuple
     activation: str = 'relu'
 
-def assemble_model(src: np.ndarray, arch_layers: list) -> keras.Model:
-    inputs = keras.Input(shape=src.shape, name='stft')
+def assemble_model(src: np.ndarray,
+                   arch_layers: list,
+                   n_dft: int=128,
+                   n_hop: int=64,) -> keras.Model:
 
-    # TODO parametrize, explain
-    n_dft: int = 128
-    n_hop: int = 64
+    inputs = keras.Input(shape=src.shape, name='stft')
 
     # @paper: Spectrogram based CNN that receives the (log) spectrogram matrix as input
 
@@ -119,9 +119,10 @@ def assemble_model(src: np.ndarray, arch_layers: list) -> keras.Model:
     # abs(Spectrogram) in a shape of 2D data, i.e.,
     # `(None, n_channel, n_freq, n_time)` if `'channels_first'`,
     # `(None, n_freq, n_time, n_channel)` if `'channels_last'`,
-    x = Spectrogram(n_dft=n_dft, n_hop=n_hop, input_shape=src.shape,
-                    trainable_kernel=True, name='static_stft',
-                    image_data_format='channels_first',)(inputs)
+    x: Spectrogram = Spectrogram(n_dft=n_dft, n_hop=n_hop, input_shape=src.shape,
+                                 trainable_kernel=True, name='static_stft',
+                                 image_data_format='channels_first',
+                                 return_decibel_spectrogram=True,)(inputs)
 
     for arch_layer in arch_layers:
         x = keras.layers.Conv2D(arch_layer.filters,
@@ -136,7 +137,10 @@ def assemble_model(src: np.ndarray, arch_layers: list) -> keras.Model:
     x = keras.layers.Dense(512)(x)
 
     # @paper: FC-368(sigmoid)
-    outputs = keras.layers.Dense(368, activation='sigmoid', name='predictions')(x)
+    x = keras.layers.Dense(368, activation='sigmoid', name='predictions')(x)
+
+    # experiment with upsampling
+    outputs = keras.layers.UpSampling2D(size=(2, 2), interpolation='nearest')(x)
 
     return keras.Model(inputs=inputs, outputs=outputs)
 
@@ -154,16 +158,36 @@ if __name__ == "__main__":
     c1: ArchLayer = ArchLayer(38, (13, 26), (13,26))
     c1_layers: list = [c1]
 
+    """Conv 2 (3 Layers)"""
+    c3_layers : list = [
+        ArchLayer(35, (6,7), (5,6)),
+        ArchLayer(87, (6,9), (5,8))
+    ]
+
+    """Conv 3 (4 Layers)"""
+    c4_layers: list = [
+        ArchLayer(32, (4,5), (3,4)),
+        ArchLayer(98, (4,6), (3,5)),
+        ArchLayer(128, (4,6), (3,5))
+    ]
+
     """Conv 6XL, 7 Layers"""
-    # TODO
+    c6XL_layers: list = [
+        ArchLayer(64, (3,3), (2,2)),
+        ArchLayer(128, (3,3), (2,2)),
+        ArchLayer(128, (3,4), (2,3)),
+        ArchLayer(128, (3,3), (2,2)),
+        ArchLayer(256, (3,3), (2,2)),
+        ArchLayer(256, (3,3), (1,2))
+    ]
 
     # @kapre: input should be a 2D array, `(audio_channel, audio_length)`.
     input_2d: np.ndarray = np.expand_dims(y_audio, axis=0)
 
     model: keras.Model = assemble_model(input_2d,
-                                        arch_layers=c1_layers,)
+                                        arch_layers=c3_layers,)
 
-    # TODO parametrize
+    # simulate dataset
     n_samples: int = 1000
     x_train: np.ndarray = np.array([input_2d] * n_samples)
     y_train: np.ndarray = np.random.uniform(size=(n_samples,) + model.output_shape[1:])
@@ -179,20 +203,22 @@ if __name__ == "__main__":
     summarize_compile(model)
 
     # Fit, with validation
+    epochs: int = 10 #100
     model: keras.Model = fit(model,
                              x_train, y_train,
                              x_val, y_val,
-                             epochs=1)
+                             epochs=epochs,)
 
     # TEMP
     if os.getenv('EXPERIMENTATION', True):
         # Freeze model
-        model.save('models/saved/dx7_sample.h5')
+        model.save(f'models/saved/DX7Sample_epochs={epochs}.h5')
 
         # Predict
         x_test: np.ndarray = np.array([input_2d] * 1)
         result: np.ndarray = predict(model, x_test, sample_rate)
 
         # Write audio
+        # TODO: is the output? inverse decibel spectrogram
         new_audio: np.ndarray = stft_to_audio(result)
-        sf.write('audio/outputs/new_audio.wav', new_audio, sample_rate)
+        librosa.output.write_wav('audio/outputs/new_audio.wav', new_audio, sample_rate)
