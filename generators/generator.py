d74252a34f332d2106359e83d7802164a8b5bb78
Dave Murray-Rust <dave@mo-seph.com>
2020-05-04 10:31:02 +0200

    Refactored generator so that it can be repeatedly re-run to make sample sets

--- generators/generator.py
+++ generators/generator.py
@@ -5,6 +5,7 @@ from typing import Dict, Tuple, Sequence, List
 #ParamValue = Tuple[str,float,List[float]]
 import random
 import os
+import os.path
 import h5py
 import argparse
 
@@ -58,73 +59,110 @@ class DatasetCreator():
         self.parameters = parameters
         self.dataset_dir = dataset_dir
         self.wave_file_dir = wave_file_dir
-        self.index = 0
         self.normalise = normalise
         os.makedirs(dataset_dir, exist_ok=True)
         os.makedirs(f"{wave_file_dir}/{name}", exist_ok=True)
 
-    def generate(self, sound_generator: SoundGenerator, length: float = 0.1,
-                 sample_rate: int = 44100, max: int = 10, method: str = 'complete', extra: dict = {}):
+    def create_parameters(self,max: int = 10, method: str = 'complete', extra: dict = {}, force_create=False) -> str :
+        filename = self.get_dataset_filename(  "data", 'hdf5')
+        if os.path.isfile(filename) and not force_create:
+            print("Parameter file exists, not recreating (use --regenerate_samples if you want to force)")
+            return filename
         print("+"*40)
-        print(f"Generating Dataset {self.name}, {max} files of {length}s at {sample_rate}/s")
-        print(f"Output waves: {self.wave_file_dir}, datasets: {self.dataset_dir}")
+        print(f"Generating Dataset {self.name}, {max} examples")
+        print(f"Datasets: {self.dataset_dir}")
         print("+"*40)
-        self.index = 0
+
+        # Save out the parameters first
+        self.save_parameters()
+
+        # Generate the set of samples (could switch to generators,
+        # but need to figure out arbitrary size arrays in HDF5)
         dataset: List[Sample] = []
         if method == "complete":
             dataset = self.parameters.recursively_generate_all()
         else:
             dataset = self.parameters.sample_space(sample_size=max)
 
-        n_samps = int(length*sample_rate)
-        #print("First sample: {}".format(dataset[0]))
-        #print("First parameters: {}".format(dict(dataset[0].value_list())))
-        records = len(dataset)
-        param_size = len(dataset[0].encode())
+        # Create the data file and add all the points to it
+        with h5py.File(filename, 'w') as datafile:
+            # Figure out the sizes to store
+            records = len(dataset)
+            param_size = len(dataset[0].encode())
 
-        datafile = h5py.File(self.get_dataset_filename(
-            dataset, "data", 'hdf5'), 'w')
-        filenames = datafile.create_dataset(
-            "files", (records,), dtype=h5py.string_dtype())
-        labels = datafile.create_dataset("labels", (records, param_size))
+            # Add columns to it
+            filenames = datafile.create_dataset(
+                "files", (records,), dtype=h5py.string_dtype())
+            parameters = datafile.create_dataset(
+                "parameters", (records,), dtype=h5py.string_dtype())
+            labels = datafile.create_dataset(
+                "labels", (records, param_size))
+            audio_exists = datafile.create_dataset(
+                "audio_exists", (records,), dtype=np.bool)
 
-        for p in dataset:
-            params = self.parameters.to_settings(p)
-            filename = self.get_wave_filename(self.index)
-            audio = sound_generator.generate(
-                params,filename,
-                length, sample_rate, extra)
 
-            filenames[self.index] = filename
-            labels[self.index] = p.encode()
-            if self.index % 1000 == 0:
-                print("Generating example {}".format(self.index))
-            self.index = self.index + 1
+            # Generate the sample points
+            for index,point in enumerate(dataset):
+                params = self.parameters.to_settings(point)
+                filenames[index] = self.get_wave_filename(index)
+                labels[index] = point.encode()
+                parameters[index] = json.dumps(params)
+                audio_exists[index] = False
+                if index % 1000 == 0:
+                    print("Generating parameters for example {}".format(index))
             datafile.flush()
         datafile.close()
-        # self.save_audio(dataset)
-        # self.save_labels(dataset)
-        self.save_parameters()
 
-    # def save_audio(self,dataset:List[Sample]):
-        #audio = tuple(t.audio for t in dataset)
-        #audio_data = np.expand_dims(np.vstack(audio), axis=1)
-        #print("Audio data: {}".format(audio_data.shape))
-        # np.save(self.get_dataset_filename(dataset,"input"),audio_data)
+        return filename
 
-    # def save_labels(self,dataset:List[Sample]):
-        #param = tuple(t.encode() for t in dataset)
-        ##param_data = np.expand_dims(np.vstack(param), axis=1)
-        #param_data = np.vstack(param)
-        #print("Param data: {}".format(param_data.shape))
-        # np.save(self.get_dataset_filename(dataset,"labels"),param_data)
+
+    def generate_audio(self,  sound_generator: SoundGenerator, length: float = 0.1,
+                 sample_rate: int = 44100, extra: dict = {},dataset_filename=None,force_generate=False):
+        if dataset_filename is None:
+            dataset_filename = self.get_dataset_filename(  "data", 'hdf5')
+
+        print("+"*40)
+        print(f"Generating Audio for Dataset {self.name} ({dataset_filename}), with {length}s at {sample_rate}/s")
+        print(f"Output waves: {self.wave_file_dir}, datasets: {self.dataset_dir}")
+        print("+"*40)
+
+
+        with h5py.File(dataset_filename, 'r+') as datafile:
+            for name,value in datafile.items():
+                print(f"{name}: {value}")
+            # Get the columns
+            filenames = datafile.get("files")
+            print(filenames)
+            parameters = datafile.get("parameters")
+            print(parameters)
+            audio_exists = datafile.get("audio_exists")
+            print(audio_exists)
+
+            num_files = len(parameters)
+
+            for index,filename in enumerate(filenames):
+                if audio_exists[index] and os.path.isfile(filename) and not force_generate:
+                    print(f"Audio exists for index {index} ({filename})")
+                else:
+                    print(f"Generating Audio for index {index} ({filename})")
+                    params = json.loads(parameters[index])
+                    audio = sound_generator.generate(
+                        params,filename,
+                        length, sample_rate, extra)
+                    audio_exists[index] = True
+                    datafile.flush()
+                if index % 1000 == 0:
+                    print("Generating example {}".format(index))
+
+
+
 
     def save_parameters(self):
         self.parameters.save_json(
-            self.get_dataset_filename(None, "params", 'json'))
-        self.parameters.save(self.get_dataset_filename(None, "params", 'pckl'))
+            self.get_dataset_filename( "params", 'json'))
+        self.parameters.save(self.get_dataset_filename( "params", 'pckl'))
 
-    def get_dataset_filename(self, dataset, type: str, extension: str = "txt") -> str:
+    def get_dataset_filename(self,  type: str, extension: str = "txt") -> str:
         return f"{self.dataset_dir}/{self.name}_{type}.{extension}"
 
     def get_wave_filename(self, index: int) -> str:
@@ -146,6 +184,10 @@ def default_generator_argparse():
                         help='Sample rate (Samples/second)')
     parser.add_argument('--sampling_method', type=str, dest='method', default='random', choices=['random'],
                         help='Method to use for generating examples. Currently only random, but may include whole space later')
+    parser.add_argument('--regenerate_samples',  action="store_true",
+                        help="Regenerate the set of points to explore if it exists (will also force regenerating audio)")
+    parser.add_argument('--regenerate_audio',  action="store_true",
+                        help="Regenerate audio files if they exists")
     return parser
 
 def generate_examples(gen:SoundGenerator,parameters:ParameterSet,args=None,extra={}):
@@ -158,12 +200,17 @@ def generate_examples(gen:SoundGenerator,parameters:ParameterSet,args=None,extra
         wave_file_dir=args.wave_dir,
         parameters=parameters
     )
-    g.generate(sound_generator=gen,
+    g.create_parameters(
+                max=args.samples,
+                method=args.method,
+                force_create=args.regenerate_samples)
+
+    g.generate_audio(
+                sound_generator=gen,
                 length=args.length,
                 sample_rate=args.sample_rate,
-                method=args.method,
-                max=args.samples,
-                extra=extra)
+                extra=extra,
+                force_generate=args.regenerate_audio | args.regenerate_samples)
 
 if __name__ == "__main__":
     gen = SoundGenerator()
@@ -175,5 +222,4 @@ if __name__ == "__main__":
                        dataset_dir="test_datasets",
                        wave_file_dir="test_waves/example/",
                        parameters=parameters)
-    g.generate(sound_generator=gen, length=1,
-               sample_rate=16384, method="random", max=30)
+    g.generate_examples(sound_generator=gen, parameters=parameters)
